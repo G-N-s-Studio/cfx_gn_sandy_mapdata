565a665ff8e73d835f13366c53bec4af24623a8c
Gusepe <120711057+Gusepe@users.noreply.github.com>
2023-12-04 15:37:37 +0100

    Main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 games { 'gta5' }
 
 author 'G&N_s Studio'
-description 'Sandy Shore Mapdata , G&Ns Clinic Center + Motel + Gabz Sheriff'
+description 'Sandy Shore Mapdata'
 version '1.0.0'
 
 dependencies {
@@ -15,6 +15,5 @@ this_is_a_map 'yes'
 escrow_ignore {
     'stream/ybn/*.ybn',
     'stream/ydr/*.ydr',
-    'stream/ymap/*.ymap',
-    'stream/ydd/*.ydd'
+    'stream/ymap/*.ymap'
 }
