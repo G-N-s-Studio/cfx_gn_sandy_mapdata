baff44c2ce2ca1739857f2eaf5be1cf331db467b
Dolu <alf.rider@hotmail.fr>
2024-01-12 16:18:53 +0100

    style: format fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,14 @@
-fx_version "cerulean"
-games { 'gta5' }
+fx_version 'cerulean'
+game 'gta5'
 
 author 'G&N_s Studio'
 description 'Sandy Shore Mapdata - Clinic Center + Burgershot + Sheriff Dept + Motel + Strip Mall'
 version '3.0.0'
 
-dependencies {
-    '/server:4960',
-    '/gameBuild:2189',
-}
+dependency '/gameBuild:2189'
 
 this_is_a_map 'yes'
 
 escrow_ignore {
-    'stream/ybn/*.ybn',
-    'stream/ydr/*.ydr',
-    'stream/ydr/*.ydd',
-    'stream/ymap/*.ymap'
+    'stream/**/*'
 }
