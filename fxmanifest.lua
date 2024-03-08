9b15a5e121f51310fb5b7f20a425f68aa88dc7cf
Gusepe <gusepe73.gaming@gmail.com>
2024-03-08 15:52:49 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,14 @@ games { 'gta5' }
 
 author 'G&N_s Studio'
 description 'Sandy Shore Mapdata_Clinic_Only'
-version '1.0.0'
+version '4.0.0'
 
 dependencies {
-    '/server:4960',
-    '/gameBuild:2189',
+    '/gameBuild:2189'
 }
 
 this_is_a_map 'yes'
 
 escrow_ignore {
-    'stream/ybn/*.ybn',
-    'stream/ydr/*.ydr',
-    'stream/ymap/*.ymap'
+    'stream/**/*'
 }
